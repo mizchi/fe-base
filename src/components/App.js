9141214f8ab53b342728d838fe3d70cf081dd027
mizchi <miz404@gmail.com>
2017-12-28 05:46:39 +0900

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import createStore from '../store/createSTore'
+import createStore from '../store/createStore'
 import Routes from '../components/Routes'
 
 export default function App() {
