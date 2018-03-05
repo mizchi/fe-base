2bb48f7ef1434f9f50389bf035863f0d8fd78e62
mizchi <miz404@gmail.com>
2018-03-05 20:25:56 +0900

    Support storybook with babel 7

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,9 @@
 /* @flow */
-module.exports = {
-  plugins: [],
-  module: {
-    rules: []
-  }
+const defaultConfig = require('../webpack.config')
+const getStorybookConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
+module.exports = (config, env) => {
+  const newConfig = getStorybookConfig(config, env)
+  newConfig.module.rules = defaultConfig.module.rules
+  return newConfig
 }
